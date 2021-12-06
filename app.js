e6654bde31a90e071e5e3454608b11d49eb40f30
wjchoi <wonjeung.choi@amorepacific.com>
2021-12-06 11:25:40 +0900

    V0

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 var pgdata = require('./pgdata');
 const app = express();
+
 const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 app.listen(port, () => console.log('Example app listening on port ${port}'));
