ac75bbfa7f8f61c4dec244d2119c710e25e92d08
wjchoi <wonjeung.choi@amorepacific.com>
2021-12-07 14:38:47 +0900

    V00

--- pgdata.js
+++ pgdata.js
@@ -66,3 +66,59 @@ module.exports.viewStatus2 = function(req, res){
     })
     res.status(200);
 }
+
+module.exports.viewStatus3 = function(req, res){
+    let query = `SELECT 
+	                test_gsal01mt.PRTNRID PARTNER_ID,
+	                test_gprt01mt.PRTNRNM PARTNER_NAME,
+	                COUNT(CSTMSEQ) "TTL",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.CSTMGRADECD='TWL0004'), 0) "TTL_SnowDiamond",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.regdate='2021-11-07' AND test_gsal01mt.CSTMGRADECD='TWL0004'), 0) "20_SnowDiamond",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.CSTMGRADECD='TWL0003'), 0) "TTL_SnowCrystal",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.regdate='2021-11-07' AND test_gsal01mt.CSTMGRADECD='TWL0003'), 0) "20_SnowCrystal",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.CSTMGRADECD='TWL0002'), 0) "TTL_SnowWater",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.regdate='2021-11-07' AND test_gsal01mt.CSTMGRADECD='TWL0002'), 0) "20_SnowWater"
+                FROM test_gsal01mt 
+                LEFT JOIN test_gprt01mt ON test_gsal01mt.PRTNRID = test_gprt01mt.PRTNRID 
+                WHERE test_gsal01mt.SALORGCD = 'TW10' AND test_gsal01mt.SALOFFCD = 'TW10'
+                GROUP BY PARTNER_ID, PARTNER_NAME
+                ORDER BY COUNT(CSTMSEQ) DESC`;
+
+    pool.query(query, (err, result) => {
+        if(err){
+            console.log('query error: '+err);
+            res.status(404);
+        }
+        console.log(result.rows);
+        return res.status(200).json(JSON.stringify(result.rows))
+    })
+    res.status(200);
+}
+
+module.exports.viewStatus4 = function(req, res){
+    let query = `SELECT 
+	                test_gsal01mt.PRTNRID PARTNER_ID,
+	                test_gprt01mt.PRTNRNM PARTNER_NAME,
+	                COUNT(CSTMSEQ) "TTL",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.CSTMGRADECD='TWL0004'), 0) "TTL_SnowDiamond",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.regdate='2021-11-07' AND test_gsal01mt.CSTMGRADECD='TWL0004'), 0) "20_SnowDiamond",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.CSTMGRADECD='TWL0003'), 0) "TTL_SnowCrystal",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.regdate='2021-11-07' AND test_gsal01mt.CSTMGRADECD='TWL0003'), 0) "20_SnowCrystal",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.CSTMGRADECD='TWL0002'), 0) "TTL_SnowWater",
+	                COALESCE( COUNT(CSTMSEQ) FILTER(WHERE test_gsal01mt.regdate='2021-11-07' AND test_gsal01mt.CSTMGRADECD='TWL0002'), 0) "20_SnowWater"
+                FROM test_gsal01mt 
+                LEFT JOIN test_gprt01mt ON test_gsal01mt.PRTNRID = test_gprt01mt.PRTNRID 
+                WHERE test_gsal01mt.SALORGCD = 'TW10' AND test_gsal01mt.SALOFFCD = 'TW10'
+                GROUP BY PARTNER_ID, PARTNER_NAME
+                ORDER BY COUNT(CSTMSEQ) DESC`;
+
+    pool.query(query, (err, result) => {
+        if(err){
+            console.log('query error: '+err);
+            res.status(404);
+        }
+        console.log(result.rows);
+        return res.status(200).json(JSON.stringify(result.rows))
+    })
+    res.status(200);
+}
